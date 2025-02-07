f1c94fed8e8d42ce595e4cbb742eebe52f4c94e1
Harshil Chaturvedi <harshilchaturvedi226@gmail.com>
2025-02-08 00:16:25 +0530

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,34 @@
+const { readFile, writeFile, write } = require('fs')
+console.log('start')
+readFile('./content/first.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.log(result)
+        
+        
+        const first = result;
+   
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.log(result)
+        const second = result;
+   
+    writeFile('./content/result-async.txt',
+      `Here is the result : ${first},${second}`,
+      {flag:'a'},(err,result) =>
+        {
+            if(err)
+               {
+                console.log(err)
+                return;
+               } 
+            console.log(result)
+        }
+    )
+    })
+})
